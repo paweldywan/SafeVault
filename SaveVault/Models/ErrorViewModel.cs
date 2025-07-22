a2ded716a97708c65f3c8665907b3097727f4157
paweld139 <pawell139139@gmail.com>
2025-07-22 22:14:32 +0200

    Add project files.

---
+++ SaveVault/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SaveVault.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
